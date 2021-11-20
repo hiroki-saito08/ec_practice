f05e4767c7f19793648dce87ed5ac43c6b8de9c4
hiroki saito <hirokisaito0813@gmail.com>
2021-11-20 17:30:21 +0900

    コンポーネントのテスト・練習

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -8,7 +8,8 @@ class ComponentTestController extends Controller
 {
     //コンポーネントの設定
     public function showComponent1(){
-        return view('tests.component-test1');
+        $message = 'メッセージ';
+        return view('tests.component-test1',compact('message'));
     }
 
     public function showComponent2(){
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,5 @@
 <!-- コンポーネントを呼び出し -->
 <x-tests.app>
+  <x-slot name="header">ヘッダー2</x-slot>
 コンポーネントテスト2
 </x-tests.app>
